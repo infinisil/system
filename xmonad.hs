7ed2465450ab02559524a236f484208f6945aa4c
Silvan Mosberger <infinisil@icloud.com>
2017-05-19 13:15:08 +0200

    Added transparency for all windows

--- xmonad.hs
+++ xmonad.hs
@@ -1,4 +1,5 @@
 import XMonad
+import Data.Monoid
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
@@ -6,6 +7,25 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 import XMonad.Prompt
 import XMonad.Prompt.Shell
+import Graphics.X11.Xlib
+import Graphics.X11.Xlib.Extras
+import GHC.Word
+
+myEventHook :: Event -> X All
+myEventHook ConfigureEvent{ ev_event_type = createNotify, ev_window = id } = do
+  spawn $ "compton-trans -w " ++ show id ++ " 50"
+  return (All True)
+myEventHook _ = return (All True)
+
+-- Makes every window transparent
+setTransparentHook :: Event -> X All
+setTransparentHook ConfigureEvent{ev_event_type = createNotify, ev_window = id} = do
+  setOpacity id opacity
+  return (All True) where
+    opacityFloat = 0.85
+    opacity = floor $ fromIntegral (maxBound :: Word32) * opacityFloat
+    setOpacity id op = spawn $ "xprop -id " ++ show id ++ " -f _NET_WM_WINDOW_OPACITY 32c -set _NET_WM_WINDOW_OPACITY " ++ show op
+setTransparentHook _ = return (All True)
 
 main = do
   xmproc <- spawnPipe "xmobar"
@@ -14,14 +34,14 @@ main = do
        , modMask = mod4Mask
        , manageHook = manageDocks <+> manageHook def
        , layoutHook = avoidStruts  $  layoutHook def
-       , handleEventHook = docksEventHook <+> handleEventHook def
+       , handleEventHook = setTransparentHook <+> docksEventHook <+> handleEventHook def
        , logHook = dynamicLogWithPP xmobarPP
                        { ppOutput = hPutStrLn xmproc
                        , ppTitle = xmobarColor "green" "" . shorten 50
                        }
        , borderWidth = 3
        , normalBorderColor = "#000000"
-       , focusedBorderColor = "#007FFF"
+       , focusedBorderColor = "#DD5500"
        }
        `additionalKeys`
        [
