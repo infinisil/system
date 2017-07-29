eac4cd067d6eafaa0e75377415ce1f936fc71c1f
Silvan Mosberger <infinisil@icloud.com>
2017-07-30 00:52:32 +0200

    Removed expriments

--- nix/ssh.nix
+++
@@ -1,9 +0,0 @@
-with import <nixpkgs> {};
-cfg: ''
-${lib.concatMapStringsSep "\n" (host: ''
-Host ${host.alias}
-	HostName ${host.name}
-	${lib.optionalString (host?user) "User ${host.user}"}
-	${lib.optionalString (host?port) "Port ${host.port}"}
-'') cfg.hosts} 
-''
