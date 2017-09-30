5fce6bc65c601604d2439c5d0e920449654ef29e
Silvan Mosberger <infinisil@icloud.com>
2017-09-30 17:11:39 +0200

    remove block

--- modules/bind.nix
+++ modules/bind.nix
@@ -6,36 +6,19 @@ let
   domain = "infinisil.io";
   ip = "139.59.149.43";
   ip6 = "2a03:b0c0:3:d0::5df6:1";
-  subdomains = [ "www" "dav" "keys" "test" "mail" "mac" ];
+  subdomains = [
+    "www"
+    "dav"
+    "keys"
+    "test"
+    "mail"
+    "mac"
+  ];
 in
 {
   networking.firewall = {
     allowedTCPPorts = [ 53 ];
     allowedUDPPorts = [ 53 ];
-  };
-
-  systemd = {
-    timers.block = {
-      partOf = [ "block.service" ];
-      wantedBy = [ "timers.target" ];
-      timerConfig.OnCalendar = "*:0/10:0"; # Every 10 minutes
-    };
-    services.block = {
-      enable = true;
-      path = with pkgs; [ iproute gawk ];
-      script = ''
-        if [[ ! -f /var/log/named/named.log ]]; then
-          exit 0
-        fi
-
-        ips=$(cat /var/log/named/named.log | grep 'cpsc.gov' | awk '{sub("#.*", "", $5);print $5;}' | sort | uniq)
-        for ip in $ips; do
-          ip route add blackhole $ip
-          echo $ip >> /var/dns/blocked
-        done
-        echo "" > /var/log/named/named.log
-      '';
-    };
   };
 
   services = {
@@ -51,20 +34,10 @@ in
         "31.165.62.80" # Fritzbox
         "195.176.96.0/19" # ETHZ
       ];
-      extraConfig = ''
-        logging {
-          channel security_file {
-              file "/var/log/named/named.log" versions 3 size 30m;
-              severity dynamic;
-              print-time yes;
-          };
-          category security {
-              security_file;
-          };
-        };
-     '';
       zones = [
         {
+          name = domain;
+          master = true;
           file = builtins.toFile domain ''
             $TTL 1d
             $ORIGIN ${domain}.
@@ -89,9 +62,6 @@ in
               "${sub} IN CNAME ${domain}."
             ) subdomains}
           '';
-
-          master = true;
-          name = domain;
         }
       ];
     };
