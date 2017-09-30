2bf2f5aa92808066da16b98160ccc8ec5497ad8a
Silvan Mosberger <infinisil@icloud.com>
2017-09-30 15:56:16 +0200

    add ethz subnet

--- modules/bind.nix
+++ modules/bind.nix
@@ -49,6 +49,7 @@ in
         "127.0.0.0/24"
         "178.197.128.0/17" # Swisscom
         "31.165.62.80" # Fritzbox
+        "195.176.96.0/19" # ETHZ
       ];
       extraConfig = ''
         logging {
