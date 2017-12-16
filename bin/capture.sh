7d7e88cf897f2d9a86b0cf77845c669a2084000f
Silvan Mosberger <infinisil@icloud.com>
2017-12-16 19:41:08 +0100

    remove some unused stuff

--- bin/capture.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -o xtrace
-INFO=$(xwininfo -frame -root)
-WIN_GEO=$(echo $INFO | grep -oEe 'geometry [0-9]+x[0-9]+' | grep -oEe '[0-9]+x[0-9]+')
-WIN_XY=$(echo $INFO | grep -oEe 'Corners:\s+\+[0-9]+\+[0-9]+' | grep -oEe '[0-9]+\+[0-9]+' | sed -e 's/\+/,/' )
-
-ffmpeg -f x11grab -y -r 30 -s $WIN_GEO -i :0.0+$WIN_XY -vcodec ffv1 -qscale 0 output.avi
