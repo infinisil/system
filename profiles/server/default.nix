bf62b406a2cc57b07b5dba9ff376b6eb1baa6544
Silvan Mosberger <infinisil@icloud.com>
2017-12-19 01:58:58 +0100

    server: auto-garbage-collection

--- profiles/server/default.nix
+++ profiles/server/default.nix
@@ -55,6 +55,11 @@
 
   nix = {
     autoOptimiseStore = true;
+    gc = {
+      automatic = true;
+      dates = "daily";
+      options = "--delete-older-than 7d";
+    };
     nixPath = [
       # Ruin the config so we don't accidentally run
       # nixos-rebuild switch on the host (thanks grahamc!)
