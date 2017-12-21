6c746a8065ea75261d301a15e1e63c3c395c652c
Silvan Mosberger <infinisil@icloud.com>
2017-12-21 01:11:21 +0100

    persist ssh connections for root also

--- modules/sshclient.nix
+++ modules/sshclient.nix
@@ -1,27 +1,34 @@
-{ nodes, ... }: {
-  home-manager.users.infinisil = {
+{ nodes, ... }:
 
-    programs.ssh = {
-      enable = true;
-      controlMaster = "auto";
-      controlPersist = "60";
-      matchBlocks = {
-        inf = {
-          hostname = "infinisil.com";
-        };
-        git = {
-          hostname = "infinisil.com";
-          user = "git";
-        };
-        laptop = {
-          hostname = "infinisil.com";
-          port = nodes.laptop.config.localserver.sshport;
-        };
-        pc = {
-          hostname = "infinisil.com";
-          port = nodes.pc.config.localserver.sshport;
-        };
+let
+  options = {
+    enable = true;
+    controlMaster = "auto";
+    controlPersist = "60";
+    matchBlocks = {
+      inf = {
+        hostname = "infinisil.com";
+      };
+      git = {
+        hostname = "infinisil.com";
+        user = "git";
+      };
+      laptop = {
+        hostname = "infinisil.com";
+        port = nodes.laptop.config.localserver.sshport;
+      };
+      pc = {
+        hostname = "infinisil.com";
+        port = nodes.pc.config.localserver.sshport;
       };
     };
   };
+
+in
+
+{
+
+  home-manager.users.infinisil.programs.ssh = options;
+  home-manager.users.root.programs.ssh = options;
+
 }
