340dcb34d4318134ed4563d2a595027f3360fa6f
Silvan Mosberger <infinisil@icloud.com>
2017-12-21 01:00:24 +0100

    update nixpkgs, znapzendzetup options, znapzend mac cfg

--- hardware/mac/default.nix
+++ hardware/mac/default.nix
@@ -23,6 +23,19 @@
 
   powerManagement.cpuFreqGovernor = lib.mkForce "powersave";
 
+  services.znapzend = {
+    pure = true;
+    zetup."main/data" = rec {
+      plan = "15min=>5min,1h=>15min,1d=>1h,1w=>1d,1m=>1w";
+      recursive = true;
+      destinations.backup = {
+        dataset = "betty/backup";
+        plan = plan + ",1y=>1m";
+        presend = "zpool import -f betty | true";
+      };
+    };
+  };
+
   home-manager.users.infinisil = {
 
     programs.htop.meters = {
