5ab1e19076849e38d3ad517235afef2d93908f7c
Silvan Mosberger <infinisil@icloud.com>
2017-12-28 02:40:18 +0100

    add server's znapzend config

--- hardware/paul/default.nix
+++ hardware/paul/default.nix
@@ -6,4 +6,28 @@
     ./hardware-configuration.nix
   ];
 
+  services.znapzend = {
+    pure = true;
+    zetup = {
+
+      "main/data/var/important" = {
+        plan = "1day=>1hour,1week=>1day,1month=>1week,1year=>1month";
+        destinations.pc = {
+          host = "pc";
+          dataset = "main/server/important";
+        };
+      };
+
+      "main/data/music" = rec {
+        plan = "1d=>1h,1w=>1d";
+        destinations.pc = {
+          plan = plan + ",1m=>1w";
+          host = "pc";
+          dataset = "main/server/music";
+        };
+      };
+
+    };
+  };
+
 }
