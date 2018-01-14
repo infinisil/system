f64bd342fefc6872d797f4d3b20f067617e55ff9
Silvan Mosberger <infinisil@icloud.com>
2018-01-14 08:38:55 +0100

    add some znc channels

--- modules/znc.nix
+++ modules/znc.nix
@@ -56,6 +56,9 @@
           "agda"
           "#dependent"
           "alacritty"
+          "bottest"
+          "haskell-ide-engine"
+          "pulseaudio"
         ];
       };
       networks.mozilla = {
