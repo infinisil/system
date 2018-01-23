9b28bd2a3bf54c6d0ea7cc26c5bab126c85afddc
Silvan Mosberger <infinisil@icloud.com>
2018-01-23 21:01:49 +0100

    use new wpa_supplicant module

--- modules/wlan.nix
+++ modules/wlan.nix
@@ -1,39 +1,36 @@
 { config, ... }:
-let
-  conf = ''
-    network={
-      ssid="infinisil"
-      psk="${config.private.passwords."wlan/iPhone"}"
-      priority=10
-    }
-    network={
-      ssid="eth-5"
-      key_mgmt=WPA-EAP
-      eap=TTLS
-      identity="msilvan"
-      password="${config.private.passwords."wlan/eth"}"
-      priority=5
-      phase2="auth=MSCHAPV2"
-    }
-    network={
-      ssid="Swisscom"
-      key_mgmt=NONE
-      priority=1
-    }
-    network={
-      ssid="FRITZ!Box 7490"
-      key_mgmt=NONE
-      priority=3
-    }
-  '';
-in
 {
-  environment.etc."wpa_supplicant.conf".text = conf;
 
-  networking = {
-    wireless.enable = true;
-    extraHosts = ''
-      192.168.1.1 swisscom.mobile
-    '';
+  networking.wireless = {
+    enable = true;
+    userControlled.enable = true;
+    networks = {
+
+      infinisil = {
+        psk = "${config.private.passwords."wlan/iPhone"}";
+        priority = 10;
+      };
+
+      eth-5 = {
+        auth = ''
+          key_mgmt=WPA-EAP
+          eap=TTLS
+          identity="msilvan"
+          password="${config.private.passwords."wlan/eth"}"
+          phase2="auth=MSCHAPV2"
+        '';
+        priority = 10;
+      };
+
+      Swisscom.priority = 5;
+
+      "FRITZ!Box 7490".priority = 3;
+
+    };
   };
+
+  networking.extraHosts = ''
+    192.168.1.1 swisscom.mobile
+  '';
+
 }
