3358852fdd194eb37b2463d60f0ee8714e3e5ace
Silvan Mosberger <infinisil@icloud.com>
2018-01-24 03:15:48 +0100

    znc: readd savebuff

--- modules/znc.nix
+++ modules/znc.nix
@@ -24,6 +24,7 @@
           "watch"
           "block_motd"
           "autoattach"
+          "savebuff ${config.private.passwords.znc-savebuff}"
         ];
         channels = let
           detached = [
