6df805e1e05c88e8e5e903ced44470315e6c15f9
Silvan Mosberger <infinisil@icloud.com>
2018-02-02 18:02:11 +0100

    use domenkozar's hie-nix instead of nixpkgs broken hie

--- modules/emacs/default.nix
+++ modules/emacs/default.nix
@@ -1,9 +1,19 @@
-{ pkgs, ... }: {
+{ pkgs, ... }: let
+
+  haskell-ide-engine = (import (pkgs.fetchFromGitHub {
+    owner = "domenkozar";
+    repo = "hie-nix";
+    rev = "42fe84af0a0bed1251774cf42805c42f01ecfa12";
+    sha256 = "1n2f8icmlsamg65j4q1wagpcnclah6ykjnnn2x45rf5i7qfdi6x0";
+  }))."hie-8.2";
+
+in
+  {
 
   home-manager.users.infinisil = {
 
     home.packages = with pkgs; [
-      haskellPackages.haskell-ide-engine
+      haskell-ide-engine
       haskell.compiler.ghc822
     ];
 
