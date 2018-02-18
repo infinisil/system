55072a788816d9536348f92afddff1cf5775287e
Silvan Mosberger <infinisil@icloud.com>
2018-02-18 01:21:23 +0100

    pkgs: add optional debug argument

--- pkgs/pkgs.nix
+++ pkgs/pkgs.nix
@@ -1,12 +1,12 @@
-{ pkgs ? import ../nixpkgs {}, lib ? pkgs.lib }:
+{ pkgs ? import ../nixpkgs {}, lib ? pkgs.lib, debug ? false }:
 
 with lib;
 
 mapAttrs' (name: type: {
   name = removeSuffix ".nix" name;
-  value = pkgs.callPackage "${./.}/${name}" {};
+  value = let file = ./. + "/${name}"; in
+    lib.callPackageWith (pkgs // { inherit debug; }) file {};
 }) (filterAttrs (name: type:
   (type == "directory" && builtins.pathExists "${toString ./.}/${name}/default.nix") ||
   (type == "regular" && hasSuffix ".nix" name && ! (name == "default.nix") && ! (name == "pkgs.nix"))
 ) (builtins.readDir ./.))
-
