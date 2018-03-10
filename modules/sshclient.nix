d3ef427b1367e7af9a50aad83124fc20465813ec
Silvan Mosberger <infinisil@icloud.com>
2018-03-10 19:10:23 +0100

    add ilias' server

--- modules/sshclient.nix
+++ modules/sshclient.nix
@@ -13,6 +13,10 @@ let
         hostname = "infinisil.com";
         user = "git";
       };
+      kuro = {
+        hostname = "kuro.desu.tk";
+        user = "infinisil";
+      };
       laptop.proxyCommand = "${pkgs.bash}/bin/bash -c '/run/wrappers/bin/ping 192.168.1.19 -w1 && ${pkgs.netcat}/bin/nc 192.168.1.19 22 || ${pkgs.netcat}/bin/nc infinisil.com ${toString nodes.laptop.config.localserver.sshport}'";
       pc.proxyCommand = "${pkgs.bash}/bin/bash -c '/run/wrappers/bin/ping 192.168.1.25 -w1 && ${pkgs.netcat}/bin/nc 192.168.1.25 22 || ${pkgs.netcat}/bin/nc infinisil.com ${toString nodes.pc.config.localserver.sshport}'";
     };
