89fea468568ae0ae4e64f185989885356ab5cd6d
Silvan Mosberger <infinisil@icloud.com>
2018-03-29 23:17:18 +0200

    move public dkim key to personal folder

--- personal/default.nix
+++ personal/default.nix
@@ -5,6 +5,7 @@
     ./znc.nix
   ];
 
+  mine.dns.dkimKey = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDHt0p6SsJkq7m/r21RiWyweuFrYmqzL82+LMGD3XW6u6gjvE7sTZ7zhH+sxLNEd09mipzi2gRVYdKd7zyXDmWgskU/wvR4FBvKKTb7HWFGsZpYnhbP9cmfj7A1Ezg8SkF8QM63JDf0NMNfWBOZMBvCZYPgqde93b3cjQf4NkYKEwIDAQAB";
 
   mine.dns.allowedNetworks = [
     "127.0.0.0/24"
