69fc7e4a9a5f2c51c183db2a4487a91344b9d0ea
Silvan Mosberger <infinisil@icloud.com>
2018-07-18 21:41:15 +0200

    NUR pkgs: Switch to whitelist

--- config/pkgs/nur/default.nix
+++ config/pkgs/nur/default.nix
@@ -1,19 +1,14 @@
 { pkgs, lib }:
-let
-  blacklist = [
-    # Require IFD:
-    "hashsearch"
-    "deluge"
-    "nix"
+{
 
-    # Requires passwords:
-    "say"
-  ];
-in
-
-lib.filterAttrs (name: val: ! lib.elem name blacklist) (
-  import ../default.nix {
+  inherit (import ../default.nix {
     inherit pkgs lib;
     passwords = false;
-  }
-)
+  })
+  arcred
+  compton-kawase
+  eclipse
+  imgurdl
+  ;
+
+}
