6f54d30148a65f933f80a033548dd7a5da2b3576
Silvan Mosberger <infinisil@icloud.com>
2018-09-17 06:15:22 +0200

    Add mpdris2

--- config/new-modules/x/taffybar.nix
+++ config/new-modules/x/taffybar.nix
@@ -10,7 +10,19 @@ in {
 
   config = mkIf config.mine.taffybar.enable {
 
+    services.upower.enable = true;
+
+    services.dbus.packages = [ pkgs.mpdris2 ];
+
     mine.xUserConfig = {
+
+      systemd.user.services.mpDris2 = {
+        Unit.Description = "mpDris2 - Music Player Daemon D-Bus bridge";
+        Service.ExecStart = "${pkgs.mpdris2}/bin/mpDris2";
+        Service.BusName = "org.mpris.MediaPlayer2.mpd";
+        Service.Environment = "MPD_HOST=${config.mine.mpdHost} MPD_PORT=${config.mine.mpdPort}";
+        Install.WantedBy = [ "default.target" ];
+      };
 
       systemd.user.services.taffybar = {
         Unit = {
