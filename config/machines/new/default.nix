f71982a2694f72c896adb15f08ecfdfa2ca2f8ab
Silvan Mosberger <infinisil@icloud.com>
2018-10-26 15:28:27 +0200

    Push music from server to laptop

--- config/machines/new/default.nix
+++ config/machines/new/default.nix
@@ -42,6 +42,14 @@
           plan = "1day=>1hour,1week=>1day,1month=>1week";
         };
       };
+      "tank/root/data/music" = {
+        plan = "1hour=>1hour";
+        destinations.emma = {
+          host = "laptop";
+          dataset = "tank/root/music";
+          plan = "1hour=>1hour";
+        };
+      };
     };
   };
 
