3a36bf4f1acdacc43e14381b0a4ef24e9fffadd5
Silvan Mosberger <infinisil@icloud.com>
2018-11-09 03:51:49 +0100

    RIP yuri

--- deploy/default.nix
+++ deploy/default.nix
@@ -36,11 +36,6 @@ in
     };
   };
 
-  #yuri = {
-  #  deployment.targetHost = "10.149.76.1";
-  #  imports = [ ../config/machines/yuri ];
-  #};
-
   new = {
     deployment.targetHost = "104.248.129.84";
     imports = [ ../config/machines/new ];
