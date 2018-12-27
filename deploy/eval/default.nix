f199528c09075ea96f449ce6dcc00dc5b8acbda2
Silvan Mosberger <infinisil@icloud.com>
2018-12-27 02:53:41 +0100

    Fix nixops eval thing with expression arguments
    Also use standard nixops instead of submodule

--- deploy/eval/default.nix
+++ deploy/eval/default.nix
@@ -1,7 +1,15 @@
 let
+  pkgs = import ../../external/nixpkgs {};
+  nixops = pkgs.nixopsUnstable;
+
   infoFile = if builtins.pathExists ./info.json
     then builtins.readFile ./info.json
     else throw "info.json file not found, run ./genInfo to generate it";
   info = builtins.fromJSON infoFile;
-  eval = import ../../external/nixops/nix/eval-machine-info.nix info;
+  info' = pkgs.lib.mapAttrs (key: value:
+    if key == "args" then
+      pkgs.lib.mapAttrs (arg: expr: import (builtins.toFile arg expr)) value
+      else value
+  ) info;
+  eval = import "${pkgs.nixopsUnstable}/share/nix/nixops/eval-machine-info.nix" info';
 in eval
