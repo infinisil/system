d4ae9948a99a5d7b1c035894712f850b23c95649
Silvan Mosberger <infinisil@icloud.com>
2018-12-28 00:38:54 +0100

    nixbot update

--- config/sources/nixbot.nix
+++ config/sources/nixbot.nix
@@ -1,6 +1,6 @@
 { pkgs }: pkgs.fetchFromGitHub {
   owner = "Infinisil";
   repo = "nixbot";
-  rev = "a10b9efbb81a2e5aa317e5ea1caeb7733a033bf9";
-  sha256 = "0irbhw8yhbq3nlvbvy1lixw934k229cr97rczq6qjn157p48nl3m";
+  rev = "7b8d2eda86387d85586f47393312bf6729a670da";
+  sha256 = "01gsawzcanqrg3w4w033a5pc2lxq7q4w40gy1lx3jmx99ldpnbkp";
 }
