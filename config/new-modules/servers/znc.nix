6aed70199a0506e22571979a49d682004e30e3a5
Silvan Mosberger <infinisil@icloud.com>
2019-03-01 19:27:44 +0100

    znc: Add main user to znc group

--- config/new-modules/servers/znc.nix
+++ config/new-modules/servers/znc.nix
@@ -54,6 +54,10 @@ in
 
   config = mkIf cfg.enable {
 
+    users.users = genAttrs config.mine.mainUsers (name: {
+      extraGroups = [ "znc" ];
+    });
+
     security.acme.certs.${config.networking.domain}.postRun = ''
       cp full.pem "${config.services.znc.dataDir}/znc.pem"
     '';
