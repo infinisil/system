45c3ea36651a2f4328c8a7474148f1c5ecb18e0a
Silvan Mosberger <infinisil@icloud.com>
2019-03-21 03:17:01 +0100

    Update FI and nixbot source

--- config/sources/futureInternetProjects.nix
+++ config/sources/futureInternetProjects.nix
@@ -1,7 +1,7 @@
 { pkgs, ethzgitlabtoken }: (pkgs.fetchgit {
   url = "https://token:${ethzgitlabtoken}@gitlab.inf.ethz.ch/OU-SINGLA/fi2019_grp6.git/";
-  rev = "a9e70e2b42befabfd24ce4bead9b36950307289f";
-  sha256 = "04129qw2v77b6qvvidd4d86irk6k9llmqpzrihvkq5x6c3j1l12f";
+  rev = "544d61fc96fe70745d3f7b1f8c4406930458a260";
+  sha256 = "1cjjs7pdfb7j46lr8i7krx3id9yn0hyi7wi81b042gkc30pchq6k";
 }) // {
-  meta.homepage = "https://gitlab.inf.ethz.ch/OU-SINGLA/fi2019_grp6.git/";
+  meta.homepage = "https://token:${ethzgitlabtoken}@gitlab.inf.ethz.ch/OU-SINGLA/fi2019_grp6.git/";
 }
--- config/sources/nixbot.nix
+++ config/sources/nixbot.nix
@@ -1,6 +1,6 @@
 { pkgs }: pkgs.fetchFromGitHub {
   owner = "Infinisil";
   repo = "nixbot";
-  rev = "1f796d293751108c6f85d0dccaca723efcb8b85a";
-  sha256 = "05gkbvqx1477jmy7q58q6prgfvy1y4xgfzr40ay4w6kv1ixcf0cd";
+  rev = "0c707946b1626e929d0e1e536f5b50172d5659a2";
+  sha256 = "0a16qnf0rn0fbc1x71jadlnwfr3j4hc9lp56gahv428snzxr9932";
 }
