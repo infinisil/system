1a3530a7a7d3208add39618f7f32396d40fcc73f
Silvan Mosberger <infinisil@icloud.com>
2019-04-24 05:22:04 +0200

    Remove firefox from my headless servers closure..

--- config/new-modules/x/browser.nix
+++ config/new-modules/x/browser.nix
@@ -9,7 +9,7 @@ with lib;
   };
 
   config = {
-    environment.systemPackages = [ config.mine.firefox ];
+    environment.systemPackages = mkIf config.services.xserver.enable [ config.mine.firefox ];
     nixpkgs.config.firefox.enableBrowserpass = true;
     # Note: extensions don't work with -bin versions of firefox
     mine.firefox = pkgs.firefox;
