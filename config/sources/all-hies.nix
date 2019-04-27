d455b2352cfab3a50fb2ded5de044818a125de86
Silvan Mosberger <infinisil@icloud.com>
2019-04-27 19:51:22 +0200

    all-hies update

--- config/sources/all-hies.nix
+++ config/sources/all-hies.nix
@@ -1,6 +1,6 @@
 { pkgs }: pkgs.fetchFromGitHub {
   owner = "infinisil";
   repo = "all-hies";
-  rev = "7957ff7306266097f4ca91db8e0af0b8d7ea4f78";
-  sha256 = "1jgqrr43favfb5fx5zfirqmjy572vihh38k5m9w3rzg6w5dq3an9";
+  rev = "beb50e4298e2438dae52ad3775e96527b572bf8d";
+  sha256 = "0z2d02sk74nmfdkdpz15qjg5f6wxmnay86dh7vvb0wds6v93a327";
 }
