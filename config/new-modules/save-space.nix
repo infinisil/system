cc3c47af171a32823181709bf00f562cc9ad6b1a
Silvan Mosberger <infinisil@icloud.com>
2019-04-30 23:33:44 +0200

    turn off gc

--- config/new-modules/save-space.nix
+++ config/new-modules/save-space.nix
@@ -6,13 +6,14 @@
 
     nix = {
       autoOptimiseStore = true;
-      gc = {
-        automatic = true;
-        # TODO: Increase interval to e.g. every 10 minutes?
-        dates = "daily";
-        # TODO: Add -v so I can see progress when it deletes links
-        options = "--delete-older-than 7d";
-      };
+      # I can't have this because of https://github.com/NixOS/nix/issues/719 :/
+      #gc = {
+      #  automatic = true;
+      #  # TODO: Increase interval to e.g. every 10 minutes?
+      #  dates = "daily";
+      #  # TODO: Add -v so I can see progress when it deletes links
+      #  options = "--delete-older-than 7d";
+      #};
     };
 
     # Taken from <nixpkgs/nixos/modules/profiles/minimal.nix>
