be7c653a1faab175d488d7a57f518d58463719ba
Silvan Mosberger <infinisil@icloud.com>
2019-10-24 06:04:03 +0200

    Remove fractal as it doesn't really work well

--- config/new-modules/matrix.nix
+++ config/new-modules/matrix.nix
@@ -4,29 +4,6 @@
 
   config = lib.mkIf config.mine.matrix.enable {
 
-    mine.userConfig = {
-
-      home.packages = [
-        pkgs.fractal
-      ];
-
-      systemd.user.services.fractal = {
-
-        Unit = {
-
-          Description = "Fractal autostart";
-          After = [ "graphical-session-pre.target" ];
-          PartOf = [ "graphical-session.target" ];
-        };
-
-        Install.WantedBy = [ "graphical-session.target" ];
-
-        Service = {
-          ExecStart = "${pkgs.fractal}/bin/fractal";
-          Restart = "always";
-        };
-      };
-    };
   };
 
 }
