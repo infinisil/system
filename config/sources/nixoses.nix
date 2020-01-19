16fabae10927372ce9a7fcd3376fbe865d42efac
Silvan Mosberger <contact@infinisil.com>
2020-01-19 07:00:40 +0100

    nixoses update

--- config/sources/nixoses.nix
+++ config/sources/nixoses.nix
@@ -1,7 +1,7 @@
 { pkgs }: pkgs.fetchFromGitHub {
   owner = "infinisil";
   repo = "nixoses";
-  rev = "1388bde04a5b37a199f64969c43f260e23fa557e";
-  sha256 = "0q4ndg4822h4adaqwpny11jpslyi8nkybcym0sid65bbsza2hf3m";
+  rev = "447eb484bf6966ae09416a875f618a6dc3ee81df";
+  sha256 = "0k0gjgvna3s87lg20jyjip5npqgj72gsidy51cbjqxhj5viiv34g";
   fetchSubmodules = true;
 }
