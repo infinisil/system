d4883024cf8299969444210ef6994ddfc5a5b1d0
Silvan Mosberger <contact@infinisil.com>
2020-01-19 05:25:24 +0100

    Update nixos-mailserver

--- config/sources/nixos-mailserver.nix
+++ config/sources/nixos-mailserver.nix
@@ -1,6 +1,6 @@
 { pkgs }: pkgs.fetchFromGitLab {
   owner = "simple-nixos-mailserver";
   repo = "nixos-mailserver";
-  rev = "ab33e87cea1e3b980b5da258ef6d75d93a39e05e";
-  sha256 = "0m5c2xrbj3cx8fi1rd33gpk6ia6m7shdkrfh08zcr63drqarbxir";
+  rev = "7bda4c4f110da5134ef9de5efe5d7e6f66bab6e6";
+  sha256 = "026wr69xf23ilxq7b1s5xxv169k6q8fq0z285mbx8jvsq5w0bbr9";
 }
