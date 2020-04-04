3522a1a2833b9fe00b509ea36bcdca19f252aae0
Silvan Mosberger <contact@infinisil.com>
2020-04-04 23:40:50 +0200

    Update IRC channels

--- config/personal/znc.nix
+++ config/personal/znc.nix
@@ -17,33 +17,19 @@
         Network.rizon = lib.mkForce null;
         Network.twitch = lib.mkForce null;
         Network.freenode.Chan = {
-          "#haskell" = { };
           "#haskell-ide-engine" = { };
           "#haskell.nix" = { };
           "#nixos" = { };
           "##nixos-anime" = { };
           "#bottest" = { };
           "#nixos-chat" = { };
-          "#idris" = { };
           "#nix-lang" = { };
-          "#nixcon" = { Detached = true; };
           "#nixos-borg" = { };
           "#nixos-dev" = { };
           "#nixos-security" = { };
           "#minecraft" = { };
           "#home-manager" = { };
-          "#pijul" = { Detached = true; };
-          "#proglangdesign" = { };
           "#nixos-officehours" = { };
-          "#nixos-officesupplies" = { };
-          "#purism" = { };
-          "#untrustix" = { };
-          "#spectrum" = { };
-          "#xmonad" = { };
-        };
-        Network.mozilla.Chan = {
-          "#rust" = { Disabled = true; };
-          "#rust-beginners" = { Disabled = true; };
         };
       };
     };
