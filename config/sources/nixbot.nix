6d36c3e45b6f34bdc5dd0123376dbc9a53a7fb5a
Silvan Mosberger <contact@infinisil.com>
2020-04-21 18:38:11 +0200

    Update nixbot

--- config/sources/nixbot.nix
+++ config/sources/nixbot.nix
@@ -1,6 +1,6 @@
 { pkgs }: pkgs.fetchFromGitHub {
   owner = "Infinisil";
   repo = "nixbot";
-  rev = "22a569bd236a293a2d513c837107dc2d612c5ca6";
-  sha256 = "137wx6xwf3w0fgxq46x5kzr6wiz6cykabq5sicvcd8v8wys9f9k9";
+  rev = "f6c71006e6646a490e504b491946543b55abe78f";
+  sha256 = "0nxaw8g6g9sm10nlhkzsmbknzjfm4b8nijfa8hhkxqwdmg4mpi9y";
 }
