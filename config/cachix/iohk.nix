d39faea787f2ff3a2b95e1ced133595849809dbb
Silvan Mosberger <contact@infinisil.com>
2020-05-12 19:59:00 +0200

    Add iohk cachix

---
+++ config/cachix/iohk.nix
@@ -0,0 +1,12 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://iohk.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo="
+    ];
+  };
+}
+    
