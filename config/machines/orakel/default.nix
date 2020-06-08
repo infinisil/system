3e82af0354e25e750eadea37cb6ea61042bb2a3f
Silvan Mosberger <contact@infinisil.com>
2020-06-09 00:35:28 +0200

    Configure orakel wireguard to be used for internet access

--- config/machines/orakel/default.nix
+++ config/machines/orakel/default.nix
@@ -8,11 +8,24 @@
 
   mine.enableUser = true;
 
+  networking.nat.enable = true;
+  networking.nat.externalInterface = "eth0";
+  networking.nat.internalInterfaces = [ "wg0" ];
+
   networking.firewall.allowedUDPPorts = [ 51820 ];
 
   networking.wireguard.interfaces.wg0 = {
     ips = [ "10.99.2.1/24" ];
     listenPort = 51820;
+
+    postSetup = ''
+      ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.99.2.0/24 -o eth0 -j MASQUERADE
+    '';
+
+    # This undoes the above command
+    postShutdown = ''
+      ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.99.2.0/24 -o eth0 -j MASQUERADE
+    '';
 
     peers = [
       {
