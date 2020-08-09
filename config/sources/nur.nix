041f61b4b8cbf6bbec47993347710bbdf85c74b2
Silvan Mosberger <contact@infinisil.com>
2020-08-10 00:06:05 +0200

    Update NUR

--- config/sources/nur.nix
+++ config/sources/nur.nix
@@ -1,6 +1,6 @@
 { fetchFromGitHub }: fetchFromGitHub {
   owner = "nix-community";
   repo = "NUR";
-  rev = "ad17dd9748703f9bb55745b47ebab0d60216a9d3";
-  sha256 = "1gzbgxmgrg6ghz4iv0pcwqmalxpbv4ihs8kqhyf989jbfymwnpbf";
+  rev = "48952195fe5019b6713dd52efd597124d2852d5a";
+  sha256 = "1x9pv8dy7j8bpmc7zwchrm1vq4ml01fr3k823iaa7lwznyrf71mj";
 }
