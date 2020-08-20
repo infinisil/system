05c036929dff4d3a601cf6e98ada654bb8120d69
Silvan Mosberger <contact@infinisil.com>
2020-08-20 03:51:11 +0200

    Add all machines to both wireguard networks

--- config/multimods/vpn-setup.nix
+++ config/multimods/vpn-setup.nix
@@ -17,6 +17,12 @@
       wireguard.publicKey = "F80/4J53cs1pwuLSGwKMtjLMzWYeQPy+QmUue+AJ2Co=";
       wireguard.privateKeyFile = "/root/wireguard-keys/private";
     };
+    clients.protos = {
+      subnetIp = "10.99.2.3";
+      wireguard.publicKey = "N+kEAyKQBMbdyY+yRAEVOb1AbWFYveUIDDZ+ni5h4yI=";
+      wireguard.privateKeyFile = "/root/wireguard-keys/private";
+      internetGateway = false;
+    };
   };
 
   vpn.networks.protos = {
@@ -32,6 +38,11 @@
     clients.vario = {
       subnetIp = "10.99.3.2";
       wireguard.publicKey = "F80/4J53cs1pwuLSGwKMtjLMzWYeQPy+QmUue+AJ2Co=";
+      wireguard.privateKeyFile = "/root/wireguard-keys/private";
+    };
+    clients.orakel = {
+      subnetIp = "10.99.3.3";
+      wireguard.publicKey = "t90gKxoMhKb1AscC1Ty8kfxPJQPzZ4tiaj0fn6NwLG4=";
       wireguard.privateKeyFile = "/root/wireguard-keys/private";
     };
   };
