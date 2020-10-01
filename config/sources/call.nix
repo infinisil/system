5b9481534c22dacba0a08a7fb7e0101a638aa77c
David Arnold <dar@xoe.solutions>
2020-09-30 23:41:24 -0500

    Avoid deceptive scope mismatch?

--- config/sources/call.nix
+++ config/sources/call.nix
@@ -1,10 +1,10 @@
 { file }: let
-  pkgs = import ../../external/nixpkgs {
+  fetchers = import ../../external/nixpkgs {
     overlays = [(self: super: {
       gitlabtoken = import ../../external/private/gitlabtoken;
       ethzgitlabtoken = import ../../external/private/ethzgitlabtoken;
     })];
   };
-in pkgs.callPackage file {} // {
+in fetchers.callPackage file {} // {
   inherit file;
 }
