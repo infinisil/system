838b30ea319e0334bd542048dd3a9a8554c6e843
milahu <milahu@gmail.com>
2021-03-29 20:17:13 +0200

    new-modules: allow to ignore files

--- config/new-modules/default.nix
+++ config/new-modules/default.nix
@@ -12,8 +12,19 @@ let
   # Collects all files of a directory as a list of strings of paths
   files = dir: collect isString (mapAttrsRecursive (path: type: concatStringsSep "/" path) (getDir dir));
 
+  # fix error: infinite recursion encountered
+  ignorePaths = [
+    "services/networking/helpers.nix"
+  ];
+
   # Filters out directories that don't end with .nix or are this file, also makes the strings absolute
-  validFiles = dir: map (file: ./. + "/${file}") (filter (file: hasSuffix ".nix" file && file != "default.nix" && ! lib.hasPrefix "x/taffybar/" file) (files dir));
+  validFiles = dir: map (file: ./. + "/${file}")
+    (filter (file:
+      hasSuffix ".nix" file
+      && file != "default.nix"
+      && ! lib.hasPrefix "x/taffybar/" file
+      && ! builtins.elem file ignorePaths
+    ) (files dir));
 
 in
 
