d0511c35fb15ae7fd52d4698197d6c8609a9aeaf
Silvan Mosberger <contact@infinisil.com>
2021-04-30 19:41:49 +0200

    Add previous minecraft server again

--- config/multimods/on-demand-minecraft-setup.nix
+++ config/multimods/on-demand-minecraft-setup.nix
@@ -1,9 +1,30 @@
 { config, ... }: {
+
   services.on-demand-minecraft.instances.main = {
     node = "protos";
     domain = "infinisil.com";
     imageConfiguration = ./minecraft.nix;
     settings = {
+      whitelist.infinisil = "01e2780a-1334-4891-95dd-506e58dcebb9";
+      digitalOcean = {
+        region = "fra1";
+        size = "g-2vcpu-8gb";
+        sshKey = "25879389";
+        volume = "8b787688-52d2-11ea-9e33-0a58ac14d123";
+        # TODO: This is nasty, make it better
+        tokenFile = config.nodes.protos.configuration.secrets.files.doauth.file;
+      };
+    };
+  };
+
+  nodes.protos.configuration.systemd.services.on-demand-minecraft-main.serviceConfig.SupplementaryGroups = "do-api";
+
+  services.on-demand-minecraft.instances.ss = {
+    node = "protos";
+    domain = "ss.infinisil.com";
+    imageConfiguration = ./minecraft.nix;
+    settings = {
+      port = 25568;
       whitelist.infinisil = "01e2780a-1334-4891-95dd-506e58dcebb9";
       digitalOcean = {
         region = "fra1";
@@ -16,7 +37,7 @@
     };
   };
 
-  nodes.protos.configuration.systemd.services.on-demand-minecraft-main.serviceConfig.SupplementaryGroups = "do-api";
+  nodes.protos.configuration.systemd.services.on-demand-minecraft-ss.serviceConfig.SupplementaryGroups = "do-api";
 
   services.on-demand-minecraft.instances.creative = {
     node = "protos";
@@ -57,5 +78,4 @@
   };
 
   nodes.protos.configuration.systemd.services.on-demand-minecraft-qbp.serviceConfig.SupplementaryGroups = "do-api";
-
 }
