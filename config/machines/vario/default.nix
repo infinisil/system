9c4e6a68e5d77152377d6d4abbea0c6974a545e6
Silvan Mosberger <contact@infinisil.com>
2021-07-18 15:00:52 +0200

    Configure japanese input method

--- config/machines/vario/default.nix
+++ config/machines/vario/default.nix
@@ -52,6 +52,19 @@ in {
     ./hardware-configuration.nix
   ];
 
+  # Only really for env vars
+  i18n.inputMethod = {
+    enabled = "fcitx5";
+    fcitx5.addons = [
+      pkgs.fcitx5-mozc
+    ];
+  };
+
+  # Would normally set this to fcitx, but kitty only supports ibus, and fcitx
+  # provides an ibus interface. Can't use ibus for e.g. QT_IM_MODULE though,
+  # because that at least breaks mumble
+  environment.variables.GLFW_IM_MODULE = "ibus";
+
   nix = {
     package = pkgs.nixFlakes;
     extraOptions = ''
