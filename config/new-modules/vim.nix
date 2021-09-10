16dd87b98e12d1891acad468d6f73d76f1d08e88
Silvan Mosberger <contact@infinisil.com>
2021-09-10 15:34:41 +0200

    Fix neovim LSP code action hotkey
    Previously always just applied the first code action, without allowing
    any choice

--- config/new-modules/vim.nix
+++ config/new-modules/vim.nix
@@ -193,9 +193,9 @@ in
         " nmap <leader>a  <Plug>(coc-codeaction-selected)
 
         " Remap keys for applying codeAction to the current buffer.
-        " nmap <leader>ac  <Plug>(coc-codeaction)
+        nmap <leader>a  <Plug>(coc-codeaction)
         " Apply AutoFix to problem on the current line.
-        nmap <leader>a  <Plug>(coc-fix-current)
+        " nmap <leader>a  <Plug>(coc-fix-current)
 
         " Map function and class text objects
         " NOTE: Requires 'textDocument.documentSymbol' support from the language server.
