2330656a00254820c85d775209fc6c48c3f54f84
Silvan Mosberger <contact@infinisil.com>
2023-04-17 03:10:00 +0200

    transmission changes, though this needs work

--- config/new-modules/transmission.nix
+++ config/new-modules/transmission.nix
@@ -4,51 +4,51 @@
 
   config = lib.mkIf config.mine.transmission.enable {
 
-    systemd.services.transmission.serviceConfig.BindPaths = [ "/var/lib/torrent/archive" ];
+    # systemd.services.transmission.serviceConfig.BindPaths = [ "/var/lib/torrent/archive" ];
 
     services.transmission = {
       enable = true;
-      home = "/var/lib/torrent";
+      # home = "/var/lib/torrent";
       settings = {
-        download-dir = "/var/lib/torrent/archive";
+        # download-dir = "/var/lib/torrent/archive";
         # script-torrent-done-enabled: Boolean (default = false) Run a script at torrent completion.
         # script-torrent-done-filename =
-        rpc-authentication-required = true;
-        rpc-password = config.private.passwords."infinisil@torrent.infinisil.com";
-        rpc-username = "infinisil";
-        rpc-whitelist-enabled = false;
-        rpc-host-whitelist-enabled = false;
+        #rpc-authentication-required = true;
+        #rpc-password = config.private.passwords."infinisil@torrent.infinisil.com";
+        #rpc-username = "infinisil";
+        #rpc-whitelist-enabled = false;
+        #rpc-host-whitelist-enabled = false;
         download-queue-size = 20;
         seed-queue-size = 20;
         incomplete-dir-enabled = false;
       };
     };
 
-    services.nginx = {
-      enable = true;
-      virtualHosts."torrent.infinisil.com" = {
-        enableACME = true;
-        forceSSL = true;
-        root = "/webroot";
-        basicAuth.infinisil = config.private.passwords."infinisil@torrent.infinisil.com";
-        locations."/".proxyPass = "http://localhost:${toString config.services.transmission.settings.rpc-port}";
-      };
-      virtualHosts."media.infinisil.com" = {
-        enableACME = true;
-        forceSSL = true;
-        basicAuth.infinisil = config.private.passwords."infinisil@media.infinisil.com";
-        root = "/var/lib/torrent";
-        extraConfig = ''
-          autoindex on;
-          charset UTF-8;
-        '';
-      };
-    };
+    # services.nginx = {
+    #   enable = true;
+    #   virtualHosts."torrent.infinisil.com" = {
+    #     enableACME = true;
+    #     forceSSL = true;
+    #     root = "/webroot";
+    #     basicAuth.infinisil = config.private.passwords."infinisil@torrent.infinisil.com";
+    #     locations."/".proxyPass = "http://localhost:${toString config.services.transmission.settings.rpc-port}";
+    #   };
+    #   virtualHosts."media.infinisil.com" = {
+    #     enableACME = true;
+    #     forceSSL = true;
+    #     basicAuth.infinisil = config.private.passwords."infinisil@media.infinisil.com";
+    #     root = "/var/lib/torrent";
+    #     extraConfig = ''
+    #       autoindex on;
+    #       charset UTF-8;
+    #     '';
+    #   };
+    # };
 
-    networking.firewall.allowedTCPPorts = [ 80 443 config.services.transmission.settings.rpc-port ];
+    # networking.firewall.allowedTCPPorts = [ 80 443 config.services.transmission.settings.rpc-port ];
 
     users.users.infinisil.extraGroups = [ "transmission" ];
-    users.users.nginx.extraGroups = [ "transmission" ];
+    #users.users.nginx.extraGroups = [ "transmission" ];
 
   };
 
